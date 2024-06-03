70ecc70e3bda61da086bf58f03e4f59797244bfb
al-shaimon <alshaimon968@gmail.com>
2024-06-03 23:57:11 +0600

    part-4

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import { Server } from 'http';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config, () => {
+    server = app.listen(config, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
@@ -15,3 +18,18 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`👿 unhandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`👿 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
